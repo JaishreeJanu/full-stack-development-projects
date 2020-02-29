174c4c6a2f54d3cf72b19e98d9d4d57e7693da2a
JaishreeJanu <janujaishree94@gmail.com>
2020-02-29 22:03:17 +0100

    code style changes

--- capstone project/starter/models.py
+++ capstone project/starter/models.py
@@ -4,10 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 from datetime import date
 
-#database_name = "bollywood"
-#database_path = "postgres://{}:{}@{}/{}".format('jaishree','','localhost:5432', database_name)
-#database_path = os.environ['DATABASE_URL']
-database_path = 'postgres://paxwvjtokrycpn:0d70426e3faaa90c7042d30305d165ac59aab21f3004ebe3a620361f583b1374@ec2-3-229-210-93.compute-1.amazonaws.com:5432/d20ktjloq6m7p0'
+# database_name = "bollywood"
+# database_path = "postgres://{}:{}@{}/{}".format('jaishree','',
+# 'localhost:5432',
+# database_name)
+# database_path = os.environ['DATABASE_URL']
+database_path = 'postgres://paxwvjtokrycpn:0d70426e3faaa90c7042d'+
+'30305d165ac59aab21f3004ebe3a620361f583b1374'+
+'@ec2-3-229-210-93.compute-1.amazonaws.com:5432/d20ktjloq6m7p0'
 
 db = SQLAlchemy()
 
@@ -15,43 +19,35 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    #db.drop_all()
-    #db.create_all()
-    #db_insert_records()
+    # db.drop_all()
+    # db.create_all()
+    # db_insert_records()
+
 
 def db_insert_records():
-    
-    #db.create_all()
 
-    new_actor = Actor(name = 'Gisele Budchen', age = 40, gender = 'Female')
-    new_movie = Movie(title = "Lessons",release_date = date.today())
+    # db.create_all()
+
+    new_actor = Actor(name='Gisele Budchen', age=40, gender='Female')
+    new_movie = Movie(title="Lessons", release_date=date.today())
 
     new_actor.insert()
     new_movie.insert()
 
-'''class Casting(db.Model):
-  __tablename__ = 'castings'
 
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), primary_key=True)
-  actor_id = db.Column(db.Integer, db.ForeignKey('actors.id'), primary_key=True)'''
-
-'''
-Movie
-'''
-class Movie(db.Model):  
+class Movie(db.Model):
     __tablename__ = 'movies'
 
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     release_date = Column(Date, nullable=False)
-    #castings = db.relationship('Casting', backref='movie', lazy='dynamic')
-  
 
     def __init__(self, title, release_date):
         self.title = title
@@ -60,7 +56,7 @@ class Movie(db.Model):
     def insert(self):
         db.session.add(self)
         db.session.commit()
-  
+
     def update(self):
         db.session.commit()
 
@@ -70,22 +66,24 @@ class Movie(db.Model):
 
     def format(self):
         return {
-        'id': self.id,
-        'title': self.title,
-        'release_date': self.release_date,
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date,
         }
+
 
 '''
 Actor
 '''
-class Actor(db.Model):  
+
+
+class Actor(db.Model):
     __tablename__ = 'actors'
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     age = Column(Integer)
     gender = Column(String)
-    #castings = db.relationship('Casting', backref='actor', lazy='dynamic')
 
     def __init__(self, name, age, gender):
         self.name = name
@@ -95,7 +93,7 @@ class Actor(db.Model):
     def insert(self):
         db.session.add(self)
         db.session.commit()
-  
+
     def update(self):
         db.session.commit()
 
@@ -103,11 +101,10 @@ class Actor(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-
     def format(self):
         return {
-        'id': self.id,
-        'name': self.name,
-        'age': self.age,
-        'gender': self.gender
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
         }
--- capstone project/starter/test_app.py
+++ capstone project/starter/test_app.py
@@ -8,37 +8,25 @@ from config import bearer_tokens
 from sqlalchemy import desc
 from datetime import date
 
-assistant_auth_header = {
-    'Authorization': bearer_tokens['casting_assistant']
-}
+assistant_auth = {"Authorization": bearer_tokens["casting_assistant"]}
 
-director_auth_header = {
-    'Authorization': bearer_tokens['casting_director']
-}
+director_auth_header = {"Authorization": bearer_tokens["casting_director"]}
 
-producer_auth_header = {
-    'Authorization': bearer_tokens['executive_producer']
-}
+producer_auth_header = {"Authorization": bearer_tokens["executive_producer"]}
 
-new_actor = {
-            'name' : 'ryan gosling',
-            'age' : 35,
-            "gender": "Male"
-        }
-new_movie = {
-    'title' : 'Lagaan',
-    'release_date': date.today()
-}
+new_actor = {"name": "ryan gosling", "age": 35, "gender": "Male"}
+new_movie = {"title": "Lagaan", "release_date": date.today()}
+
 
 class CastingTestCase(unittest.TestCase):
-
     def setUp(self):
 
         self.app = create_app()
         self.client = self.app.test_client
-        #database_name = "bollywood-test"
-        #database_path = "postgres://{}:{}@{}/{}".format('jaishree','dabli123','localhost:5432', database_name)
-        database_path = os.environ['DATABASE_URL']
+        # database_name = "bollywood-test"
+        # database_path = "postgres://{}:{}@{}/{}".format('jaishree',\
+        # 'dabli123','localhost:5432', database_name)
+        database_path = os.environ["DATABASE_URL"]
         setup_db(self.app, database_path)
         # binds the app to the current context
 
@@ -48,252 +36,255 @@ class CastingTestCase(unittest.TestCase):
             # create all tables
             self.db.create_all()
 
-    
     def tearDown(self):
         """Executed after each test"""
         pass
 
-    
+    # tests for POST /actors
 
-    #tests for POST /actors
     def test_add_actor(self):
-        #add new actors: success scenario 
-        res = self.client().post('/actors', json = new_actor, headers = director_auth_header)
+        # add new actors: success scenario
+        res = self.client().post(
+            "/actors", json=new_actor, headers=director_auth_header
+        )
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertEqual(data['actor'], 2)
+        self.assertTrue(data["success"])
+        self.assertEqual(data["actor"], 2)
 
     def test_add_actor_401(self):
-        #add new actor: failure ,without authorization header 
-        res = self.client().post('/actors', json = new_actor)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code,401)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'], 'Authorization header is expected.')
-
-    def test_add_actor_400(self):
-        #add new actor: failure , without 'name' in json
-        this_actor={
-            'age':45
-        }
-        res = self.client().post('/actors', json = this_actor, headers=director_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code,400)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'], 'Please add "name" in the json')
-
-    #tests for GET /actors
-    def test_get_actors(self):
-        #get actors at page=1
-        res = self.client().get('/actors?page=1', headers = assistant_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertEqual(len(data['actors']),2)
-
-    def test_get_actors_404(self):
-        #get actors at page=100
-        res = self.client().get('/actors?page=100', headers = assistant_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 404)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'],'OOPS! No actors willing to work')
-
-    def test_get_actors_401(self):
-        #get actors: failure ,without authorization header 
-        res = self.client().get('/actors')
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code,401)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'], 'Authorization header is expected.')
-
-    #tests for PATCH /actors
-    def test_modify_actors(self):
-        #update an actor , sending id and json
-        this_actor = {
-            'name':'Priyanka Chopra'
-        }
-        res = self.client().patch('/actors/1', json = this_actor , headers = producer_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertEqual(data['actor'],1)
-
-    def test_modify_actors_400(self):
-        #update an actor , not sending json
-        res = self.client().patch('/actors/1', headers = producer_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 400)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'],'there is no json body')
-
-    def test_modify_actors_403(self):
-        #update an actor , sending assistant header
-        this_actor = {
-            'name':'Priyanka Chopra'
-        }
-        res = self.client().patch('/actors/1', json = this_actor, headers = assistant_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 403)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'],'Permission not found.')
-
-    #tests for DELETE /actors
-    def test_delete_actor_404(self):
-        #delete an actor , failure scenario: incorrect actor_id
-        res = self.client().delete('/actors/10', headers = producer_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 404)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'],'No actor with this id')
-
-    def test_delete_actor_401(self):
-        #delete an actor , without headers
-        res = self.client().delete('/actors/1')
+        # add new actor: failure ,without authorization header
+        res = self.client().post("/actors", json=new_actor)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 401)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'],'Authorization header is expected.')
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "Authorization header is expected.")
 
-    def test_delete_actor(self):
-        #delete an actor:success scenario
-        res = self.client().delete('/actors/1', headers = producer_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertEqual(data['actor'],1)
-
-
-    #tests for POST /movies
-    def test_add_movie(self):
-        #add new movies: success scenario 
-        res = self.client().post('/movies', json = new_movie, headers = producer_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertEqual(data['movie'], 2)
-
-    def test_add_movie_401(self):
-        #add new movie: failure ,without authorization header 
-        res = self.client().post('/movies', json = new_movie)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code,401)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'], 'Authorization header is expected.')
-
-    def test_add_movie_400(self):
-        #add new actor: failure , without json
-        res = self.client().post('/movies', headers = producer_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code,400)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'], 'there is no json body')
-
-    #tests for GET /movies
-    def test_get_movies(self):
-        #get movies at page=1
-        res = self.client().get('/movies?page=1', headers = assistant_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertEqual(len(data['movies']),2)
-
-    def test_get_movies_404(self):
-        #get movies at page=100
-        res = self.client().get('/movies?page=100', headers = assistant_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 404)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'],'OOPS! No one is making movies')
-
-    def test_get_movies_401(self):
-        #get movies: failure ,without authorization header 
-        res = self.client().get('/movies')
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code,401)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'], 'Authorization header is expected.')
-
-    #tests for PATCH /movies
-    def test_modify_movies(self):
-        #update an movie , sending id and json
-        this_movie = {
-            'title':'mahabharta'
-        }
-        res = self.client().patch('/movies/1', json = this_movie , headers = producer_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertEqual(data['movie'],1)
-
-    def test_modify_movies_400(self):
-        #update an movie , not sending json
-        res = self.client().patch('/movies/1', headers = producer_auth_header)
+    def test_add_actor_400(self):
+        # add new actor: failure , without 'name' in json
+        this_actor = {"age": 45}
+        res = self.client().post(
+            "/actors", json=this_actor, headers=director_auth_header
+        )
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 400)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'],'there is no json body')
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], 'Please add "name" in the json')
 
-    def test_modify_movies_403(self):
-        #update an movie , sending assistant header
-        this_movie = {
-            'title':'mahabharta'
-        }
-        res = self.client().patch('/movies/1', json = this_movie, headers = assistant_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 403)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'],'Permission not found.')
-
-    #tests for DELETE /movies
-    def test_delete_movie_404(self):
-        #delete an movie , failure scenario: incorrect movie_id
-        res = self.client().delete('/movies/10', headers = producer_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 404)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'],'No movie with this id')
-
-    def test_delete_movie_403(self):
-        #delete an actor , failure senario, sending director header
-        res = self.client().delete('/movies/1', headers = director_auth_header)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 403)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'],'Permission not found.')
-
-    def test_delete_movie(self):
-        #delete an movie:success scenario
-        res = self.client().delete('/movies/1', headers = producer_auth_header)
+    # tests for GET /actors
+    def test_get_actors(self):
+        # get actors at page=1
+        res = self.client().get("/actors?page=1", headers=assistant_auth)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertEqual(data['movie'],1)
+        self.assertTrue(data["success"])
+        self.assertEqual(len(data["actors"]), 2)
+
+    def test_get_actors_404(self):
+        # get actors at page=100
+        res = self.client().get("/actors?page=100", headers=assistant_auth)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "OOPS! No actors willing to work")
+
+    def test_get_actors_401(self):
+        # get actors: failure ,without authorization header
+        res = self.client().get("/actors")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "Authorization header is expected.")
+
+    # tests for PATCH /actors
+    def test_modify_actors(self):
+        # update an actor , sending id and json
+        this_actor = {"name": "Priyanka Chopra"}
+        res = self.client().patch(
+            "/actors/1", json=this_actor, headers=producer_auth_header
+        )
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data["success"])
+        self.assertEqual(data["actor"], 1)
+
+    def test_modify_actors_400(self):
+        # update an actor , not sending json
+        res = self.client().patch("/actors/1", headers=producer_auth_header)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "there is no json body")
+
+    def test_modify_actors_403(self):
+        # update an actor , sending assistant header
+        this_actor = {"name": "Priyanka Chopra"}
+        res = self.client().patch(
+            "/actors/1", json=this_actor, headers=assistant_auth
+        )
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 403)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "Permission not found.")
+
+    # tests for DELETE /actors
+    def test_delete_actor_404(self):
+        # delete an actor , failure scenario: incorrect actor_id
+        res = self.client().delete("/actors/10", headers=producer_auth_header)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "No actor with this id")
+
+    def test_delete_actor_401(self):
+        # delete an actor , without headers
+        res = self.client().delete("/actors/1")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "Authorization header is expected.")
+
+    def test_delete_actor(self):
+        # delete an actor:success scenario
+        res = self.client().delete("/actors/1", headers=producer_auth_header)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data["success"])
+        self.assertEqual(data["actor"], 1)
+
+    # tests for POST /movies
+
+    def test_add_movie(self):
+        # add new movies: success scenario
+        res = self.client().post(
+            "/movies", json=new_movie, headers=producer_auth_header
+        )
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data["success"])
+        self.assertEqual(data["movie"], 2)
+
+    def test_add_movie_401(self):
+        # add new movie: failure ,without authorization header
+        res = self.client().post("/movies", json=new_movie)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "Authorization header is expected.")
+
+    def test_add_movie_400(self):
+        # add new actor: failure , without json
+        res = self.client().post("/movies", headers=producer_auth_header)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "there is no json body")
+
+    # tests for GET /movies
+    def test_get_movies(self):
+        # get movies at page=1
+        res = self.client().get("/movies?page=1", headers=assistant_auth)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data["success"])
+        self.assertEqual(len(data["movies"]), 2)
+
+    def test_get_movies_404(self):
+        # get movies at page=100
+        res = self.client().get("/movies?page=100", headers=assistant_auth)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "OOPS! No one is making movies")
+
+    def test_get_movies_401(self):
+        # get movies: failure ,without authorization header
+        res = self.client().get("/movies")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "Authorization header is expected.")
+
+    # tests for PATCH /movies
+    def test_modify_movies(self):
+        # update an movie , sending id and json
+        this_movie = {"title": "mahabharta"}
+        res = self.client().patch(
+            "/movies/1", json=this_movie, headers=producer_auth_header
+        )
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data["success"])
+        self.assertEqual(data["movie"], 1)
+
+    def test_modify_movies_400(self):
+        # update an movie , not sending json
+        res = self.client().patch("/movies/1", headers=producer_auth_header)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "there is no json body")
+
+    def test_modify_movies_403(self):
+        # update an movie , sending assistant header
+        this_movie = {"title": "mahabharta"}
+        res = self.client().patch(
+            "/movies/1", json=this_movie, headers=assistant_auth
+        )
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 403)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "Permission not found.")
+
+    # tests for DELETE /movies
+    def test_delete_movie_404(self):
+        # delete an movie , failure scenario: incorrect movie_id
+        res = self.client().delete("/movies/10", headers=producer_auth_header)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "No movie with this id")
+
+    def test_delete_movie_403(self):
+        # delete an actor , failure senario, sending director header
+        res = self.client().delete("/movies/1", headers=director_auth_header)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 403)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "Permission not found.")
+
+    def test_delete_movie(self):
+        # delete an movie:success scenario
+        res = self.client().delete("/movies/1", headers=producer_auth_header)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data["success"])
+        self.assertEqual(data["movie"], 1)
+
 
 if __name__ == "__main__":
     unittest.main()
